19731e2d4b961d54a2be0573b49cf239dab994ce
Damian Barczyk <damian@alfatv.pl>
2019-01-29 20:22:55 +0000

    Prepare for test

---
+++ tests/ApiTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Damian Barczyk
+ * Date: 29/01/2019
+ * Time: 20:22
+ */
+
+namespace App\Test;
+
+
+class ApiTest
+{
+    /*
+    1.	Get a list of football teams in given league
+    2.	Create a football team
+    3.	Replace all attributes of a football team
+    4.	Delete a football league
+    */
+
+
+
+}
