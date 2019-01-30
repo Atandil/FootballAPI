2d4f27ab876aaee09934b90f95d9564351197919
Damian Barczyk <damian@alfatv.pl>
2019-01-30 11:12:47 +0000

    configuraton symfony

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -8,15 +8,41 @@
 
 namespace App\Test;
 
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\DataFixtures\TeamFixtures;
 
-class ApiTest
+class ApiTest extends WebTestCase
 {
+    private $em;
+
+    protected function setUp()
+    {
+        parent::setUp(); //
+        $client = static::createClient();
+        $container = $client->getContainer();
+        $doctrine = $container->get('doctrine');
+        $entityManager = $doctrine->getManager();
+        $this->em=$entityManager;
+
+
+        $fixture = new TeamFixtures();
+        $fixture->load($entityManager);
+
+    }
     /*
     1.	Get a list of football teams in given league
     2.	Create a football team
     3.	Replace all attributes of a football team
     4.	Delete a football league
     */
+
+    public function testIndex()
+    {
+        $this->assertTrue(true);
+    }
+
+
+
 
 
 
